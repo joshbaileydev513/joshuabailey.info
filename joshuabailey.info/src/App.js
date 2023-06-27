bebf5922cc4064c1dbd7c692e0e62e699214e42f
joshbaileydev513 <joshbaileydev513@gmail.com>
2023-06-27 00:05:21 -0400

    fixed issue when starting the app with the react dom and navbar path

--- joshuabailey.info/src/App.js
+++ joshuabailey.info/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from './components/header.js';
 import Footer from './components/footer.js';
-import Navbar from './components/navbar.js';
+import Navbar from './components/nav.js';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
