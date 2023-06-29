b371494abbcd918471b68f6230f4ba78a2c5810f
joshbaileydev513 <joshbaileydev513@gmail.com>
2023-06-29 13:50:49 -0400

    installed react-bootstrap dep along with getting the navbar reactive and almost finished. still a little bit of styling left

--- joshuabailey.info/src/components/nav.js
+++ joshuabailey.info/src/components/nav.js
@@ -1,45 +1,29 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 // We declare an object called styles that will contain a few objects for card and heading styles
 // Notice that each key lists CSS styles in camel case
-const styles = {
-  logo: {
-    display: 'flex',
-    background: '#9a74db',
-    width: '100%',
-    alignContent: 'space-between',
-  },
 
-  nav: {
-    display: 'flex',
-    alignItems: 'flex-end',
-    flexDirection: 'row',
-    listStyleType: 'none',
-    width: 'auto',
-  },
-
-  split: {
-    display: 'inline-block',
-  }
-};
-
-// In Navbar, we can assign a style from an object by using curly braces
-function Navbar() {
+function NavComp() {
   return (
-    <nav style= {styles.logo}>
-      {/* NavBar Content Goes here - Contact/Portfolio/Resume */}
-      {/* create a separate styling for JB */}
-      <h3 style= {styles.split}> <a href="*">JB</a></h3>
-      <div style= {styles.split}>
-        <ul style={styles.nav}>
-          {/* <li><a href="*"> Home </a></li> */}
-          <li><a href="/portfolio"> Portfolio </a></li>
-          <li><a href="/resume"> Resume</a></li>
-          <li><a href="/contact"> Contact </a></li>
-        </ul>
-      </div>
-    </nav>
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="*">JB</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+          </Nav>
+          <Nav>
+            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="/resume">Resume</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default NavComp;
