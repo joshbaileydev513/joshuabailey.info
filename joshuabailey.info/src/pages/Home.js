fc5acd019149ca9cfe7639eb3358b7554aa96f97
joshbaileydev513 <joshbaileydev513@gmail.com>
2023-06-30 23:52:26 -0400

    tweaked assets folder and added an images subfolder on it. Still trying to get the image to show

--- joshuabailey.info/src/pages/Home.js
+++ joshuabailey.info/src/pages/Home.js
@@ -1,12 +1,11 @@
 import React from 'react';
-// import joshPic from './src/assets/home-page-pic.png'
 
 const styles = {
     background: {
         backgroundColor: '#BEBEBE',
     },
     // pic: {
-    //     backgroundImage: 'url(./home-page-pic.png)',
+    //     backgroundImage: 'url(./assets/images/home-page-pic.png)',
     // },
 };
 
@@ -14,9 +13,8 @@ const Home = () => {
     return (
         <div class="h-100 d-inline-block" style={styles.background}>
             {/* Inset Picture of Me here */}
-            {/* <img src={joshPic} alt="Josh Pic" /> */}
             <h3 class="p-5 text-center">FullStack Developer - MERN </h3>
-             {/* Add info here about me and desires/exp */}
+            {/* <img src="./public/home-page-pic.png" alt="Josh Pic" /> */}
              <p class="text-center font-weight-light">
                     Hi there! I am a FullStack Developer within MERN, currently enrolled in a coding bootcamp at The University
                     of North Carolina at Charlotte. I am passionate about creating visually appealing and user-friendly
