ad0137eec35dbc4491f962f6ccde83a9193d7e2a
joshbaileydev513 <joshbaileydev513@gmail.com>
2023-07-01 01:33:38 -0400

    fixed contact page spacing

--- joshuabailey.info/src/pages/Contact.js
+++ joshuabailey.info/src/pages/Contact.js
@@ -7,6 +7,7 @@ const Contact = () => {
              {/* Add contact info (PH #/ EM / GitHub / LinkedIn) */}
              <div class="p-5 text-center">
                 <button onClick={() => window.location = 'mailto:jbailey2124@gmail.com'} type="button" class="btn btn-outline-dark">EMAIL</button>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <a href="tel:5135185570"><button type="button" class="btn btn-outline-dark">CALL</button></a>
              </div>
         </div>
