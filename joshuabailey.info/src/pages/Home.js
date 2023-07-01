5e832a98c7f100ab4e3b80fe746e964fc4e55dfa
joshbaileydev513 <joshbaileydev513@gmail.com>
2023-07-01 01:02:02 -0400

    minor commit

--- joshuabailey.info/src/pages/Home.js
+++ joshuabailey.info/src/pages/Home.js
@@ -8,7 +8,6 @@ const styles = {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
-        // width: '50%', /*or whatever you choose*/
         margin: 'auto',
     },
 };
