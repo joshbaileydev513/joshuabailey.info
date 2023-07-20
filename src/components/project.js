3ba96df2ba14b97689e52be69e54cfcf53abd0d6
Joshua Bailey <joshbaileydev513@gmail.com>
2023-07-20 00:33:05 -0400

    fixed title of web browser along with added two portfolio pics. Very minorly styled but got somewhat of the basic idea of what I want it to look like

--- src/components/project.js
+++ src/components/project.js
@@ -1,35 +1,51 @@
 import React from 'react';
 // Here we are importing a CSS file as a dependency
 // import '../styles/Header.css';
-import HomePic from '../assets/home-page-pic.png'
+import ArcadeAssem from '../assets/arcadeassembly.png';
+import BobsBurgers from '../assets/bobs-burgers.png';
+import HomePic from '../assets/home-page-pic.png';
 
+const styles = {
+    picFormat: {
+      display: 'flex',
+      flexDirection: 'row',
+      borderRadius: '15px',
+      padding: '15px',
+      margin: '15px',
+      gap: '89px',
+    },
+    size: {
+      height: 'auto',
+      width: '100px'
+    }
+};
 
 function Projects() {
   return (
-    <section>
+    <section style={styles.picFormat}>
       {/* Best Portfolio Piece Here */}
       <div>
-        <img src={HomePic} alt="Josh Pic" />
+        <img src={ArcadeAssem} alt="Arcade Assembly" style={styles.size}/>
       </div>
       {/* Portfolio Project #2 */}
       <div>
-        <img src={HomePic} alt="Josh Pic" />
+        <img src={BobsBurgers} alt="Bobs Burgers API" style={styles.size}/>
       </div>
       {/* Portfolio Project #3 */}
       <div>
-        <img src={HomePic} alt="Josh Pic" />
+        <img src={HomePic} alt="Josh Pic" style={styles.size}/>
       </div>
       {/* Portfolio Project #4 */}
       <div>
-        <img src={HomePic} alt="Josh Pic" />
+        <img src={HomePic} alt="Josh Pic" style={styles.size}/>
       </div>
       {/* Portfolio Project #5 */}
       <div>
-        <img src={HomePic} alt="Josh Pic" />
+        <img src={HomePic} alt="Josh Pic" style={styles.size}/>
       </div>
       {/* Portfolio Project #6 */}
       <div>
-        <img src={HomePic} alt="Josh Pic" />
+        <img src={HomePic} alt="Josh Pic" style={styles.size}/>
       </div>
     </section>
   );
