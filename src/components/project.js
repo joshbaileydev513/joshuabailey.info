76c4d386ec6399f3af669593811be951696957a9
Joshua Bailey <joshbaileydev513@gmail.com>
2023-07-31 23:56:48 -0400

    fixed the styling of the buttons to match the other pages

--- src/components/project.js
+++ src/components/project.js
@@ -4,37 +4,36 @@ import Link from 'react-router-dom';
 // import '../styles/Header.css';
 import ArcadeAssem from '../assets/arcadeassembly.png';
 import BobsBurgers from '../assets/bobs-burgers.png';
-import HomePic from '../assets/home-page-pic.png';
 import BrickThat from '../assets/brick-that-pic.png';
 import TextEditor from '../assets/text-editor.png';
 import ReadmeGen from '../assets/readme-gen.png';
 import NoteTaker from '../assets/note-taker.png';
 
 const styles = {
-    picFormat: {
-      display: 'flex',
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'center'
-    },
-    size: {
-      borderRadius: '50px',
-      height: '300px',
-      width: '300px',
-      border: '2px solid #555555',
-      padding: '10px',
-      margin: '10px'
-    },
-    btn: {
-      textAlign: 'center',
-      backgroundColor: '#555555',
-      color: 'white',
-      borderRadius: '12px',
-    },
-    btnCenter: {
-      display: 'flex',
-      justifyContent: 'center'
-    }
+  picFormat: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  },
+  size: {
+    borderRadius: '50px',
+    height: '300px',
+    width: '300px',
+    border: '2px solid #555555',
+    padding: '10px',
+    margin: '10px'
+  },
+  btn: {
+    textAlign: 'center',
+    backgroundColor: '#555555',
+    color: 'white',
+    borderRadius: '12px',
+  },
+  btnCenter: {
+    display: 'flex',
+    justifyContent: 'center'
+  }
 };
 
 function Projects() {
@@ -43,59 +42,59 @@ function Projects() {
       {/* Best Portfolio Piece Here */}
       <div>
         <a href="https://arcade-assembly-c977bb949035.herokuapp.com/" target="_blank" rel="noreferrer">
-          <img src={ArcadeAssem} alt="Arcade Assembly" style={styles.size}/>
-          <br/>
+          <img src={ArcadeAssem} alt="Arcade Assembly" style={styles.size} />
+          <br />
           <div style={styles.btnCenter}>
-            <button type="button" style={styles.btn}>GitHub Repo</button>
+            <button type="button" class="btn btn-outline-dark">GitHub Repo</button>
           </div>
         </a>
       </div>
       {/* Portfolio Project #2 */}
       <div>
         <a href="https://brick-that.herokuapp.com/" target="_blank" rel="noreferrer">
-          <img src={BrickThat} alt="Brick That" style={styles.size}/>
-            <br/>
-              <div style={styles.btnCenter} >
-                <button type="button" style={styles.btn}>GitHub Repo</button>
-              </div>
+          <img src={BrickThat} alt="Brick That" style={styles.size} />
+          <br />
+          <div style={styles.btnCenter} >
+            <button type="button" class="btn btn-outline-dark">GitHub Repo</button>
+          </div>
         </a>
       </div>
       {/* Portfolio Project #3 */}
       <div>
         <a href="https://joshbaileydev513.github.io/bobs-burgers-project/" target="_blank" rel="noreferrer">
-          <img src={BobsBurgers} alt="Bobs Burgers API" style={styles.size}/>
-          <br/>
-              <div style={styles.btnCenter} >
-                <button type="button" style={styles.btn}>GitHub Repo</button>
-              </div>
+          <img src={BobsBurgers} alt="Bobs Burgers API" style={styles.size} />
+          <br />
+          <div style={styles.btnCenter} >
+            <button type="button" class="btn btn-outline-dark">GitHub Repo</button>
+          </div>
         </a>
       </div>
       {/* Portfolio Project #4 */}
       <div>
-        <img src={TextEditor} alt="Text Editor PWA" style={styles.size}/>
-        <br/>
-              <div style={styles.btnCenter} >
-                <button type="button" style={styles.btn}>GitHub Repo</button>
-              </div>
+        <img src={TextEditor} alt="Text Editor PWA" style={styles.size} />
+        <br />
+        <div style={styles.btnCenter} >
+          <button type="button" class="btn btn-outline-dark">GitHub Repo</button>
+        </div>
       </div>
       {/* Portfolio Project #5 */}
       <div>
         <a href="https://note-taker-app-uncc.herokuapp.com/" target="_blank" rel="noreferrer">
-          <img src={NoteTaker} alt="Note Taker App" style={styles.size}/>
-          <br/>
-              <div style={styles.btnCenter} >
-                <button type="button" style={styles.btn}>GitHub Repo</button>
-              </div>
+          <img src={NoteTaker} alt="Note Taker App" style={styles.size} />
+          <br />
+          <div style={styles.btnCenter} >
+            <button type="button" class="btn btn-outline-dark">GitHub Repo</button>
+          </div>
         </a>
       </div>
       {/* Portfolio Project #6 */}
       <div>
         <a href="https://app.screencastify.com/manage/videos/xrR4u9qmVMsZMdTKu5oa" target="_blank" rel="noreferrer">
-          <img src={ReadmeGen} alt="readme generator" style={styles.size}/>
-          <br/>
-              <div style={styles.btnCenter} >
-                <button type="button" style={styles.btn}>GitHub Repo</button>
-              </div>
+          <img src={ReadmeGen} alt="readme generator" style={styles.size} />
+          <br />
+          <div style={styles.btnCenter} >
+            <button type="button" class="btn btn-outline-dark">GitHub Repo</button>
+          </div>
         </a>
       </div>
     </section>
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -8,8 +8,7 @@ const Resume = () => {
              <p class="text-center font-weight-light">
                     Enthusiastic, and driven development professional with a passion for working diligently to complete
                     any tasks at hand. Avid learner who is willing to put in the time needed to successfully pick up new
-                    skills needed within any role.Currently enrolled in the Coding Bootcamp at the University of North Carolina Charlotte learning a
-                    full stack of tools and technologies.
+                    skills needed within any role. FullStack MERN Certication from The University of North Carolina at Charlotte Coding Bootcamp.
             </p>
             <div>
             <div class="p-5 text-center">
