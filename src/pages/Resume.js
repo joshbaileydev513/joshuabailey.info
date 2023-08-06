7aac07605cbb28c9b4d82e0c627f7128f0d17719
Joshua Bailey <joshbaileydev513@gmail.com>
2023-08-05 20:36:05 -0400

    trying to get my pdf to load in resume page. very close

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import { Document, Page } from 'react-pdf';
+import ResumePdf from '../joshua-bailey-res-2023.pdf';
 
 const Resume = () => {
     return (
         <div>
             <h3 class="p-5 text-center">Resume</h3>
-             {/* Add Resume Below */}
-             <p class="text-center font-weight-light">
-                    Enthusiastic, and driven development professional with a passion for working diligently to complete
-                    any tasks at hand. Avid learner who is willing to put in the time needed to successfully pick up new
-                    skills needed within any role. FullStack MERN Certication from The University of North Carolina at Charlotte Coding Bootcamp.
+            <Document file={ResumePdf}>
+                <Page pageNumber={1} />
+            </Document>
+            <p class="text-center font-weight-light">
+                Enthusiastic, and driven development professional with a passion for working diligently to complete
+                any tasks at hand. Avid learner who is willing to put in the time needed to successfully pick up new
+                skills needed within any role. FullStack MERN Certication from The University of North Carolina at Charlotte Coding Bootcamp.
             </p>
             <div>
-            <div class="p-5 text-center">
-                {/* need to fix the file location for my resume PDF */}
-                <a href={Resume.pdf} target="_blank"><button type="button" class="btn btn-outline-dark">Download PDF Here</button></a>
-            </div>
+                <div class="p-5 text-center">
+                    {/* need to fix the file location for my resume PDF */}
+                    <a href={Resume.pdf} target="_blank"><button type="button" class="btn btn-outline-dark">Download PDF Here</button></a>
+                </div>
             </div>
         </div>
     );
