be5f4db26eaec7e99db2f98501cbe098d8ffdd59
Joshua Bailey <joshbaileydev513@gmail.com>
2023-08-16 23:57:42 -0400

    got PDF to show, now need get it to not put it in text below it

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -7,7 +7,7 @@ const Resume = () => {
     const [numPages, setNumPages] = useState(null);
 
 
-    pdfjs.GlobalWorkerOptions.workerSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js';
+    pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
     
     function onDocumentLoadSuccess({ numPages }) {
         setNumPages(numPages);
