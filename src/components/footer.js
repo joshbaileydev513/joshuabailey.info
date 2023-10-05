574a80c4768d5b2a2c415e6815e6011903bfb862
Joshua Bailey <joshbaileydev513@gmail.com>
2023-10-05 00:03:36 -0400

    fixed links and stlying in regards to centering & on diff lines

--- src/components/footer.js
+++ src/components/footer.js
@@ -17,6 +17,7 @@ const styles = {
     color: '#333',
     fontSize: '24px',
     margin: '0 10px',
+    paddingTop: '10px',
   },
 };
 
@@ -25,17 +26,19 @@ function Footer() {
     <footer class="p-3 mb-2 bg-dark text-white text-center font-weight-light" style= {styles.footer}>
       <h6>"Uncertainty and mystery are energies of life. 
         Do not let them scare you unduly, for they keep boredom at bay and spark creativity"
-      </h6>
         {/* put a few links at the bottom for my linkedIn/github etc */}
-        <div stlye={styles.icons}>
-          <a href="https://github.com/joshbaileydev513" target="_blank">
-            <i className="fa fa-github"></i>
-          </a>
-          <a href="https://www.linkedin.com/in/joshuacbailey/" target="_blank">
-            <i className="fa fa-linkedin"></i>
-          </a>
-          {/* Add more icons as needed */}
+          <div>
+            <div style={styles.icons}>
+                <a href="https://github.com/joshbaileydev513" target="_blank">
+                  <i className="fa fa-github"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/joshuacbailey/" target="_blank">
+                  <i className="fa fa-linkedin"></i>
+                </a>
+              {/* Add more icons as needed */}
+            </div>
         </div>
+      </h6>
     </footer>
   );
 }
