069990e08d611ee284f0d7d3cb36f0446db9cbf1
Joshua Bailey <joshbaileydev513@gmail.com>
2023-10-17 19:02:32 -0400

    updated paragraph on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,7 +30,7 @@ const Home = () => {
                 <img style={styles.border} src={HomePic} alt="Josh Pic"/>
             </div>
              <p class="text-center font-weight-light" style={styles.textPadding}>
-                    Hi there, I am a Developer with a FullStack MERN Certication from The University of North Carolina at Charlotte Coding Bootcamp. I am passionate about creating visually appealing and user-friendly
+                    Hi there, I am a Fullstack Developer &#40;MERN&#41; with a passion for creating visually appealing and user-friendly
                     websites and applications. My goal is to use my skills and knowledge to create meaningful and impactful digital
                     experiences for users. In my free time, I enjoy staying up to date with the latest technology and trends in the industry
                     and participating in coding challenges. I am excited to continue learning and growing as a developer, and I am eager to
